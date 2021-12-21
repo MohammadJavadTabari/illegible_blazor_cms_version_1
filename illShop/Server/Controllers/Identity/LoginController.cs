3b2b2e59031b2687b694fc3bbf17d974c7e14ef0
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2021-12-21 06:26:56 -0800

    .

--- illShop/Server/Controllers/Identity/LoginController.cs
+++ illShop/Server/Controllers/Identity/LoginController.cs
@@ -23,69 +23,81 @@ namespace illShop.Server.Controllers.Identity
             _signInManager = signInManager;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Login([FromBody] LoginModelDto login)
-        {
-            // so simple
-            // i try to sign in User with password
-            var result = await _signInManager.PasswordSignInAsync(login.Email,login.Password, false, false);
+        //[HttpPost]
+        //public async Task<IActionResult> Login([FromBody] LoginModelDto login)
+        //{
+        //    // so simple
+        //    // i try to sign in User with password
+        //    var result = await _signInManager.PasswordSignInAsync(login.Email,login.Password, false, false);
 
-            // in case of error 
-            if (!result.Succeeded) return BadRequest(new LoginResultDto
-            {
-                Successful = false,
-                Error = "Username or password are invalid."
-            });
-
-
-            // claim defult is on email
-            // cause user name is the same as email
-            // you can change it or set it dynamic
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.Name, login.Email)
-            };
-
-            // const data from kernel (read this class comments)
-            var jwtSetting = new JwtSetting();
-            // this is what i love in this code
-            // we generate SigningCredential Key
-            // with encoding our jwt long private key in utf8
-            var key = new SymmetricSecurityKey(Encoding
-                .UTF8
-                .GetBytes(jwtSetting.SecuritySignInKey));
-
-            // create SigningCredentials by
-            // hashing  generated key i defined above in Hmac256
-            // (i call it pretty little kido Cryptography algorithm)
-            var creds = new SigningCredentials(key,
-                SecurityAlgorithms.HmacSha256);
-
-            // set token expire time for one day
-            var expiry = DateTime.Now.AddDays
-                (Convert.ToInt32(1));
+        //    // in case of error 
+        //    if (!result.Succeeded) return BadRequest(new LoginResultDto
+        //    {
+        //        Successful = false,
+        //        Error = "Username or password are invalid."
+        //    });
 
 
-            var token = new JwtSecurityToken(
-                //jwt issuer
-                jwtSetting.ValidIssuer,
-                //jwt audience
-                jwtSetting.ValidAudience,
-                claims,
-                expires: expiry,
-                signingCredentials: creds
-            );
+        //    // claim defult is on email
+        //    // cause user name is the same as email
+        //    // you can change it or set it dynamic
+        //    var claims = new[]
+        //    {
+        //        new Claim(ClaimTypes.Name, login.Email)
+        //    };
 
-            //serialize jwt token
-            var jweToken = new JwtSecurityTokenHandler()
-                .WriteToken(token);
+        //    // const data from kernel (read this class comments)
+        //    var jwtSetting = new JwtSetting();
+        //    // this is what i love in this code
+        //    // we generate SigningCredential Key
+        //    // with encoding our jwt long private key in utf8
+        //    var key = new SymmetricSecurityKey(Encoding
+        //        .UTF8
+        //        .GetBytes(jwtSetting.SecuritySignInKey));
 
-            // and there we go returning LoginResultDto with token
-            return Ok(new LoginResultDto
-            {
-                Successful = true,
-                Token = jweToken
-            });
-        }
+        //    // create SigningCredentials by
+        //    // hashing  generated key i defined above in Hmac256
+        //    // (i call it pretty little kido Cryptography algorithm)
+        //    var creds = new SigningCredentials(key,
+        //        SecurityAlgorithms.HmacSha256);
+
+        //    // set token expire time for one day
+        //    var expiry = DateTime.Now.AddDays
+        //        (Convert.ToInt32(1));
+
+
+        //    var token = new JwtSecurityToken(
+        //        //jwt issuer
+        //        jwtSetting.ValidIssuer,
+        //        //jwt audience
+        //        jwtSetting.ValidAudience,
+        //        claims,
+        //        expires: expiry,
+        //        signingCredentials: creds
+        //    );
+
+        //    //serialize jwt token
+        //    var jweToken = new JwtSecurityTokenHandler()
+        //        .WriteToken(token);
+
+        //    // and there we go returning LoginResultDto with token
+        //    return Ok(new LoginResultDto
+        //    {
+        //        Successful = true,
+        //        Token = jweToken
+        //    });
+        //}
+        //[HttpPost("Login")]
+        //public async Task<IActionResult> Login([FromBody] UserForAuthenticationDto userForAuthentication)
+        //{
+        //    var user = await _userManager.FindByNameAsync(userForAuthentication.Email);
+        //    if (user == null || !await _userManager.CheckPasswordAsync(user, userForAuthentication.Password))
+        //        return Unauthorized(new AuthResponseDto { ErrorMessage = "Invalid Authentication" });
+        //    var signingCredentials = GetSigningCredentials();
+        //    var claims = GetClaims(user);
+        //    var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+        //    var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        //    return Ok(new AuthResponseDto { IsAuthSuccessful = true, Token = token });
+        //}
     }
 }
