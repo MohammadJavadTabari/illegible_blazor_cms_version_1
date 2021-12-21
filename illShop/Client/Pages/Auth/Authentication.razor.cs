89f85da627228cb9fa8c406ec4eae4575191b5e2
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2021-12-21 00:30:38 -0800

    cleaning

---
+++ illShop/Client/Pages/Auth/Authentication.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace illShop.Client.Pages.Auth
+{
+    public partial class Authentication
+    {
+        [Parameter] public string Action { get; set; }
+    }
+}
---
+++ illShop/Client/Pages/Auth/RedirectToLogin.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace illShop.Client.Pages.Auth
+{
+    public partial class RedirectToLogin
+    {
+        protected override void OnInitialized()
+        {
+            NavigationManager.NavigateTo($"authentication/login?returnUrl={Uri.EscapeDataString(NavigationManager.Uri)}");
+        }
+    }
+}
