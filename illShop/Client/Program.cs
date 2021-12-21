0594a4e5a05d274f71d99a980a0e3bbc2bbfc76f
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2021-12-21 03:42:35 -0800

    add login api

--- illShop/Client/Program.cs
+++ illShop/Client/Program.cs
@@ -12,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<IHttpRequestHandlerService, HttpRequestHandlerService>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddFileReaderService(o => o.UseWasmSharedBuffer = false);
 
 await builder.Build().RunAsync();
---
+++ illShop/Server/Controllers/Identity/AccountsController.cs
@@ -0,0 +1,35 @@
+﻿using illShop.Shared.Dto.DtosRelatedIdentity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace illShop.Server.Controllers.Identity
+{
+    [Route("AccountHandelMethods")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AccountsController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        [Route("Registration")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            if (userForRegistration == null || !ModelState.IsValid)
+                return BadRequest();
+            var user = new IdentityUser { UserName = userForRegistration.Email, Email = userForRegistration.Email };
+
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+                return BadRequest(new RegistrationResponseDto { Errors = errors });
+            }
+
+            return StatusCode(201);
+        }
+    }
+}
