7770f3b42f54dd5605f6c6b4a274e59b851791de
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2021-12-21 06:00:27 -0800

    cleaning

---
+++ illShop/Shared/Dto/DtosRelatedIdentity/LoginResultDTO.cs
@@ -0,0 +1,11 @@
+﻿
+namespace illShop.Shared.Dto.DtosRelatedIdentity
+{
+    public class LoginResultDto
+    {
+        public bool Successful { get; set; }
+        public string Error { get; set; }
+        public string Token { get; set; }
+
+    }
+}
