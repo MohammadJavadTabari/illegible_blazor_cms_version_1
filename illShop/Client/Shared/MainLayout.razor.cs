0e4b3b368de60eac1497d0e41987ecafdc411cd0
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2021-12-22 09:36:47 -0800

    CLEANING OBJECTS

--- illShop/Client/Shared/MainLayout.razor.cs
+++ illShop/Client/Shared/MainLayout.razor.cs
@@ -4,41 +4,9 @@ namespace illShop.Client.Shared
 {
     public partial class MainLayout
     {
-        private bool _isLightMode = true;
         private MudTheme _currentTheme = new MudTheme();
-        private void ToggleTheme()
-        {
-            _isLightMode = !_isLightMode;
-            if (!_isLightMode)
-            {
-                _currentTheme = GenerateDarkTheme();
-            }
-            else
-            {
-                _currentTheme = new MudTheme();
-            }
-        }
-        private MudTheme GenerateDarkTheme() =>
-        new MudTheme
-        {
-            Palette = new Palette()
-            {
-                Black = "#27272f",
-                Background = "#32333d",
-                BackgroundGrey = "#27272f",
-                Surface = "#373740",
-                TextPrimary = "#ffffffb3",
-                TextSecondary = "rgba(255,255,255, 0.50)",
-                AppbarBackground = "#27272f",
-                AppbarText = "#ffffffb3"
-            }
-        };
-        private bool _sidebarOpen = true;
-        
-
-        private void ToggleNavMenu()
-        {
-            _sidebarOpen = !_sidebarOpen;
-        }
+        private bool _sidebarOpen = false;
+        private void ToggleTheme(MudTheme changedTheme) => _currentTheme = changedTheme;
+        private void ToggleSidebar() => _sidebarOpen = !_sidebarOpen;
     }
 }
---
+++ illShop/Client/Shared/NavMenu.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace illShop.Client.Shared
+{
+    public partial class NavMenu
+    {
+        [Parameter]
+        public bool SideBarOpen { get; set; }
+    }
+}
