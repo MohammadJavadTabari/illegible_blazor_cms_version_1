ef14d22ec7182697b05c3e3bc310bb7984c85308
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2021-12-24 07:15:25 -0800

    add product detail

--- illShop/Client/Pages/ProductComponents/ProductDetail.razor.cs
+++ illShop/Client/Pages/ProductComponents/ProductDetail.razor.cs
@@ -1,32 +1,37 @@
-﻿//using illShop.Shared.Dto.DtosRelatedProduct;
-//using Microsoft.AspNetCore.Components;
+﻿using illShop.Shared.Dto.DtosRelatedProduct;
+using Microsoft.AspNetCore.Components;
 
-//namespace illShop.Client.Pages.ProductComponents
-//{
-//    public partial class ProductDetail
-//    {
-//        public ProductDto Product { get; set; } = new();
-       
-//        [Parameter]
-//        public int ProductId { get; set; }
+namespace illShop.Client.Pages.ProductComponents
+{
+    public partial class ProductDetail
+    {
+        public ProductDto Product { get; set; } = new();
 
-//        private int _currentRating;
-//        private int _reviewCount;
-//        private int _questionCount;
+        [Parameter]
+        public int ProductId { get; set; }
 
-//        protected async override Task OnInitializedAsync()
-//        {
-//            Product = await _httpRequestHandler.GetById<ProductDto>(ProductId, "ProductHandlers/GetProductById");
-//            _reviewCount = Product.Reviews.Count;
-//            _questionCount = Product.QAs.Count;
-//            _currentRating = CalculateRating();
-//        }
-//        private int CalculateRating()
-//        {
-//            var rating = Product.Reviews.Any() ? Product.Reviews.Average(r => r.Rate) : 0;
-//            return Convert.ToInt32(Math.Round(rating));
-//        }
-//        private void RatingValueChanged(int value) =>
-//            Console.WriteLine($"The product is rated with {value} thumbs.");
-//    }
-//}
+        private int _currentRating;
+        private int _reviewCount;
+        private int _questionCount;
+
+        protected async override Task OnInitializedAsync()
+        {
+            Product = await _httpRequestHandler.GetById<ProductDto>(ProductId, "ProductHandlers/GetProductById");
+            
+            if (Product.ProductReviews != null)
+            {
+                var reviews = Product.ProductReviews;
+                _reviewCount = reviews.Count() ;
+                _questionCount =reviews.Where(x => string.IsNullOrWhiteSpace(x.UserComment)).Count();
+                _currentRating = CalculateRating(reviews);
+            }
+        }
+        private int CalculateRating(List<ProductReviewDto> reviews)
+        {
+            var rating = reviews.Any() ? reviews.Average(r => r.Rate) : 0;
+            return Convert.ToInt32(Math.Round(rating));
+        }
+        private void RatingValueChanged(int value) =>
+            Console.WriteLine($"The product is rated with {value} thumbs.");
+    }
+}
