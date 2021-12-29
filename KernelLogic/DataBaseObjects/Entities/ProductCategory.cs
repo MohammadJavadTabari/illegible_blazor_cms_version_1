90fbce0dc1b89f5f70f299bef283aa3574f93039
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2021-12-29 06:15:43 -0800

    migration

--- KernelLogic/DataBaseObjects/Entities/ProductCategory.cs
+++ KernelLogic/DataBaseObjects/Entities/ProductCategory.cs
@@ -1,16 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KernelLogic.DataBaseObjects.Entities
 {
     public class ProductCategory : BaseEntity
     {
         public string? CategoryName { get; set; }
+        public string? Icon { get; set; }
         public List<Product>? Products { get; set; }
     }
     public class ProductCategoryConfiguration : IEntityTypeConfiguration<ProductCategory>
@@ -18,6 +14,7 @@ namespace KernelLogic.DataBaseObjects.Entities
         public void Configure(EntityTypeBuilder<ProductCategory> builder)
         {
             builder.Property(b => b.CategoryName).IsRequired();
+            builder.Property(b => b.Icon).IsRequired();
         }
     }
 }
---
+++ KernelLogic/Migrations/20211229141503_AddIconToCategory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KernelLogic.Migrations
+{
+    public partial class AddIconToCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Icon",
+                schema: "dbo",
+                table: "ProductCategory",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Icon",
+                schema: "dbo",
+                table: "ProductCategory");
+        }
+    }
+}
