466fd053c43c7d95b974c06932b45a161d2939f8
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2021-12-29 07:46:49 -0800

    add fontawse

--- illShop/Shared/Dto/DtosRelatedProduct/ProductCategoryDto.cs
+++ illShop/Shared/Dto/DtosRelatedProduct/ProductCategoryDto.cs
@@ -1,9 +1,14 @@
-﻿namespace illShop.Shared.Dto.DtosRelatedProduct
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace illShop.Shared.Dto.DtosRelatedProduct
 {
     public class ProductCategoryDto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="please fill categry name input")]
         public string? CategoryName { get; set; }
+        [Required(ErrorMessage = "please select icon ")]
+        public string? Icon { get; set; }
         public List<ProductDto>? Products { get; set; }
     }
 }
