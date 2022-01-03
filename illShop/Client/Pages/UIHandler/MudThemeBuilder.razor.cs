3d516e75052bb281ba86c4b9f97e7511b08207d7
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-01-03 12:09:01 -0800

    themeBuilder part 1

---
+++ illShop/Client/Pages/UIHandler/MudThemeBuilder.razor.cs
@@ -0,0 +1,46 @@
+﻿using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace illShop.Client.Pages.UIHandler
+{
+    public partial class MudThemeBuilder
+    {
+        public MudColor _gradientPrimary = "#594AE2";
+        public MudColor _gradientSecondary = "#FF4081";
+        public MudColor _pickerColor = "#594AE2";
+
+        int degrees = 90;
+
+        bool _isFirstColor = true;
+
+        public void ChangeSelectedColor(MudListItem item)
+        {
+            if (item.Text == "1")
+            {
+                _isFirstColor = true;
+                _pickerColor = _gradientPrimary;
+                UpdateSelectedColor(_gradientPrimary);
+            }
+            else if (item.Text == "2")
+            {
+                _isFirstColor = false;
+                _pickerColor = _gradientSecondary;
+                UpdateSelectedColor(_gradientSecondary);
+            }
+        }
+
+        public void UpdateSelectedColor(MudColor value)
+        {
+            _pickerColor = value;
+
+            if (_isFirstColor)
+            {
+                _gradientPrimary = value;
+            }
+            else
+            {
+                _gradientSecondary = value;
+            }
+        }
+    }
+}
