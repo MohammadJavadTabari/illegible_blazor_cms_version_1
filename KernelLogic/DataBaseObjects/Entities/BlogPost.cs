d0fb767ca0c8852268d7ec2c83ac2ddfa1e38cb3
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-01-04 13:48:28 -0800

    add blog post entity

---
+++ KernelLogic/DataBaseObjects/Entities/BlogPost.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KernelLogic.DataBaseObjects.Entities
+{
+    public class BlogPost : BaseEntity
+    {
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Summary { get; set; }
+        public DateTime WriteTime { get; set; }
+        public string? PostContext { get; set; }
+        public string? PostImageUrl { get; set; }
+        public string? PostVideoUrl { get; set; }
+        public string? PostAttachedLinkUrl { get; set; }
+        public string? PostAttachedLinkUrlSubject { get; set; }
+
+    }
+
+    public class BlogPostConfiguration : IEntityTypeConfiguration<BlogPost>
+    {
+        public void Configure(EntityTypeBuilder<BlogPost> builder)
+        {
+            #region Required
+
+            builder.Property(b => b.Title)
+                .IsRequired();
+
+            builder.Property(b => b.Author)
+               .IsRequired();
+
+            builder.Property(b => b.Summary)
+               .IsRequired();
+
+            builder.Property(b => b.WriteTime)
+              .IsRequired();
+
+            builder.Property(b => b.PostContext)
+                .IsRequired();
+
+            #endregion
+        }
+    }
+}
---
+++ KernelLogic/Migrations/20220104214733_AddBlogPost.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KernelLogic.Migrations
+{
+    public partial class AddBlogPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlogPost",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WriteTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PostContext = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostVideoUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostAttachedLinkUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostAttachedLinkUrlSubject = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogPost", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogPost",
+                schema: "dbo");
+        }
+    }
+}
