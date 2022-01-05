65e6afc41db785e05a36897d2ed9a79fdf348acf
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-01-04 23:41:12 -0800

    add blog postdto

---
+++ illShop/Shared/Dto/DtosRelatedBlog/BlogPostDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace illShop.Shared.Dto.DtosRelatedBlog
+{
+    public class BlogPostDto
+    {
+        [Required]
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        [Required]
+        public string? Summary { get; set; }
+        public DateTime WriteTime { get; set; }
+        [Required]
+        public string? PostContext { get; set; }
+        [Url]
+        public string? PostImageUrl { get; set; }
+        [Url]
+        public string? PostVideoUrl { get; set; }
+        [Url]
+        public string? PostAttachedLinkUrl { get; set; }
+        public string? PostAttachedLinkUrlSubject { get; set; }
+    }
+}
