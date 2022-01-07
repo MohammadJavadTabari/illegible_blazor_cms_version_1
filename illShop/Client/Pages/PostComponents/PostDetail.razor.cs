02ecde53997ce00d5eba7c7173a1a01f8ca6abbd
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-01-07 07:25:10 -0800

    add pd

---
+++ illShop/Client/Pages/PostComponents/PostDetail.razor.cs
@@ -0,0 +1,12 @@
+﻿using illShop.Shared.Dto.DtosRelatedBlog;
+using Microsoft.AspNetCore.Components;
+
+namespace illShop.Client.Pages.PostComponents
+{
+    public partial class PostDetail
+    {
+        [Parameter]
+        public int Id { get; set; }
+        private readonly BlogPostDto blogPostDto = new();
+    }
+}
