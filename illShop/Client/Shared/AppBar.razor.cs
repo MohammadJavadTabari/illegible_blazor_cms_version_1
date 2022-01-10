bbc967998aef6b0958210bea4095a898d5ef038b
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-01-09 22:51:53 -0800

    cleaning

--- illShop/Client/Shared/AppBar.razor.cs
+++ illShop/Client/Shared/AppBar.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MudBlazor;
+using MudBlazor.Utilities;
 
 namespace illShop.Client.Shared
 {
@@ -17,5 +18,6 @@ namespace illShop.Client.Shared
             _currentTheme = _themeCustomazition.SelectTheme(ThemeId);
             await OnThemeToggled.InvokeAsync(_currentTheme);
         }
+        public IEnumerable<MudColor> Palette4 { get; set; } = new MudColor[] { "#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5" };
     }
 }
--- illShop/Client/Shared/ExtensionServices/IThemeCustomazition.cs
+++ illShop/Client/Shared/ExtensionServices/IThemeCustomazition.cs
@@ -54,6 +54,102 @@ namespace illShop.Client.Shared.ExtensionServices
                     DrawerText = "#424242ff",
                     DrawerIcon = "#616161ff"
                 }
+            },
+            ///////////////////////////////////////
+             // green
+            new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Background = "#F44336",
+                    BackgroundGrey = "#E91E63",
+                    Surface = "#00995e",
+                    AppbarBackground = "#673AB7",
+                    DrawerBackground = "#3F51B5",
+                    DrawerText = "#E8EAF6",
+                    DrawerIcon = "#616161ff"
+                }
+            },
+            // green
+            new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Background = "#FFEBEE",
+                    BackgroundGrey = "#FCE4EC",
+                    Surface = "#FCE4EC",
+                    AppbarBackground = "#F3E5F5",
+                    DrawerBackground = "#F3E5F5",
+                    DrawerText = "#EDE7F6",
+                    DrawerIcon = "#E8EAF6"
+                }
+            },
+            // green
+            new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Background = "#FFCDD2",
+                    BackgroundGrey = "#F8BBD0",
+                    Surface = "#E1BEE7",
+                    AppbarBackground = "#D1C4E9",
+                    DrawerBackground = "#D1C4E9",
+                    DrawerText = "#C5CAE9",
+                    DrawerIcon = "#E8EAF6"
+                }
+            },
+            // green
+            new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Background = "#EF9A9A",
+                    BackgroundGrey = "#EF9A9A",
+                    Surface = "#F48FB1",
+                    AppbarBackground = "#CE93D8",
+                    DrawerBackground = "#CE93D8",
+                    DrawerText = "#B39DDB",
+                    DrawerIcon = "#9FA8DA"
+                }
+            },
+            new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Background = "#E57373",
+                    BackgroundGrey = "#E57373",
+                    Surface = "#F06292",
+                    AppbarBackground = "#BA68C8",
+                    DrawerBackground = "#BA68C8",
+                    DrawerText = "#9575CD",
+                    DrawerIcon = "#7986CB"
+                }
+            },
+            new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Background = "#EF5350",
+                    BackgroundGrey = "#EF5350",
+                    Surface = "#EC407A",
+                    AppbarBackground = "#AB47BC",
+                    DrawerBackground = "#AB47BC",
+                    DrawerText = "#7E57C2",
+                    DrawerIcon = "#5C6BC0"
+                }
+            },
+            new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Background = "#E53935",
+                    BackgroundGrey = "#E53935",
+                    Surface = "#D81B60",
+                    AppbarBackground = "#8E24AA",
+                    DrawerBackground = "#8E24AA",
+                    DrawerText = "#5E35B1",
+                    DrawerIcon = "#3949AB"
+                }
             }
         };
         public MudTheme SelectTheme(string themeId)
