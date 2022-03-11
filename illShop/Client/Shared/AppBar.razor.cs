a17acff7a51bf2c4caa1ffb8ae4bef17e9ceec9e
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-03-11 05:35:48 -0800

    .

--- illShop/Client/Shared/AppBar.razor.cs
+++ illShop/Client/Shared/AppBar.razor.cs
@@ -18,7 +18,6 @@ namespace illShop.Client.Shared
             _currentTheme = _themeCustomazition.SelectTheme(ThemeId);
             await OnThemeToggled.InvokeAsync(_currentTheme);
         }
-        public IEnumerable<MudColor> Palette4 { get; set; } = new MudColor[] { "#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5" };
         
         
         [Parameter]
